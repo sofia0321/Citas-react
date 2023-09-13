2f29b5607b1cdfe41c696770a8041c044cfc23a2
Sofía Gómez <Sofa Gmez>
2023-09-13 17:24:24 -0500

    cuarta clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import ListadoPacientes from "./components/ListadoPacientes"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
+import { useState } from "react"
 
 function App() {
+
+  const[pacientes, setPacientes]= useState(
+    [
+      {
+      nombre: " lll", 
+      propietario: " hhh",
+      email:" huhhu",
+      fecha: " uidhuisdhuidhui",
+      sintomas:" kjsuisui"
+      }
+    ]
+    )
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
 
       <div className="mt-12 md:flex">
-        <Formulario />
-        <ListadoPacientes />
+        <Formulario pacientes={pacientes}  setPacientes={setPacientes}/>
+        <ListadoPacientes pacientes={pacientes} />
       </div>
 
     </div>
---
+++ src/components/Alerta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alerta = ({error, mensaje}) => {
+  return (
+    <div className=
+    {`${error ? 'bg-red-600':'bg-indigo-600'} 
+    text-white text-center p-3 uppercase font-bold mb-3 rounded-lg`}>{mensaje}</div>
+  )
+}
+
+export default Alerta
